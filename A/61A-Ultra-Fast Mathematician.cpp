979cdddf87831d422fbff7c885af4ea79bf3aae3
Ahmed <ahmedfayez37@gmail.com>
2018-12-11 19:34:58 +0200

    Add files via upload

---
+++ A/61A-Ultra-Fast Mathematician.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <bitset>
+
+int main()
+{
+	std::string s1, s2;
+
+	std::cin >> s1 >> s2;
+
+	for(int i = 0; i < s1.length(); i++)
+	{
+		std::cout << (s1[i] == s2[i] ? '0' : '1');
+	}
+
+	return 0;
+}
